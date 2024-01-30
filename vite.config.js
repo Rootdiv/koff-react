74cff3c6a7d980c0a67e38e68a00cc0561e47c7b
Rootdiv <rootdiv9152@gmail.com>
2024-01-30 16:52:04 +0300

    fix: compatible vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'node:path';
+import { fileURLToPath, URL } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(import.meta.dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
   build: {
