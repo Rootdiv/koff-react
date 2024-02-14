b817293f0e9991b56b95f9d47f79b582d6a7fead
Rootdiv <rootdiv9152@gmail.com>
2024-02-14 22:20:06 +0300

    feat: added active link in the catalog

--- src/views/Catalog/Catalog.jsx
+++ src/views/Catalog/Catalog.jsx
@@ -3,11 +3,14 @@ import style from './Catalog.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCategories } from '@/store/categories/categoriesSlice';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import clsx from 'clsx';
 
 export const Catalog = () => {
   const dispatch = useDispatch();
+  const [searchParam] = useSearchParams();
   const { data, loading, error } = useSelector(state => state.categories);
+  const category = searchParam.get('category');
 
   useEffect(() => {
     dispatch(fetchCategories());
@@ -21,7 +24,7 @@ export const Catalog = () => {
       <ul className={style.list}>
         {data.map((item, i) => (
           <li className={style.item} key={i}>
-            <Link className={style.link} to={`/category?category=${item}`}>
+            <Link className={clsx(style.link, category === item && style.linkActive)} to={`/category?category=${item}`}>
               {item}
             </Link>
           </li>
